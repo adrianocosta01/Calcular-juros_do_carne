903fe520841dea38f5feefdb0d896cb3efedabde
Adriano Costa <159786050+adrianocosta01@users.noreply.github.com>
2025-05-12 23:43:02 -0300

    Adiciona cálculos de multa e juros por atraso, com formatação monetária BRL
    Implementei os cálculos para multa (1%) e juros (0.1% ao dia) com base na diferença de dias entre o vencimento e o pagamento. Utilizei a API Intl.NumberFormat para formatar os valores monetários no padrão BRL. A lógica foi aplicada à entrada do pagamento, com verificação de data de vencimento e ajustes no formato de exibição. Os resultados agora são mostrados de forma adequada ao usuário, considerando o formato monetário brasileiro.

--- script.js
+++ script.js
@@ -41,21 +41,35 @@ nao_tem_parcelamento.addEventListener('change', () => {
 let dia_do_pagamento = document.querySelector('#dia_do_pagamento')
 let botao_calcular = document.querySelector('#botao_calcular')
 
-
+let formatador = new Intl.NumberFormat('pt-BR', { 
+    style: 'currency', 
+    currency: 'BRL'
+})
 
 botao_calcular.addEventListener('click', () => {
 
-    let data_do_pagamento = new Date(dia_do_pagamento.value)
+    let pagou_dia = new Date(dia_do_pagamento.value)
+    pagou_dia.setHours(0, 0, 0, 0)
 
     if (tem_entrada.checked) {
         let data_do_vencimento_entrada = document.querySelector('#vencimento_da_entrada')
-        let valor_da_entrada = document.querySelector('#valor_da_entrada')  
-        //teste
+        let venceu_dia = new Date(data_do_vencimento_entrada.value)
+        venceu_dia.setHours(0, 0, 0, 0)
 
-        
+        let valor_da_entrada = document.querySelector('#valor_da_entrada').value
+        let pagou_entrada = parseFloat(valor_da_entrada.replace(',','.'))
+
+        let atraso_da_entrada = (pagou_dia.getTime() - venceu_dia.getTime()) / (1000 * 60 * 60 * 24)
+
+        let multa_da_entrada = (1*pagou_entrada/100)
+        let juros_da_entrada = ((0.1*pagou_entrada/100) * (atraso_da_entrada))
+
+        let total_da_entrada = (multa_da_entrada+juros_da_entrada)
         let p1 = window.document.querySelector('#teste')
 
-        p1.innerHTML = `Data do Vencimento da Entrada: ${data_do_pagamento}}`
+        p1.innerHTML = `Dias de vencimento: ${atraso_da_entrada} Dias <br> Multa ${formatador.format(multa_da_entrada)} <br>
+        Juros ${formatador.format(juros_da_entrada)} <br>
+        Total: ${formatador.format(total_da_entrada)}`
         
     }
 
