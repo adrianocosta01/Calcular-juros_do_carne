7266a897b0ab0cf767879593429c3ddba5ffad81
Adriano Costa <159786050+adrianocosta01@users.noreply.github.com>
2025-05-22 23:31:09 -0300

    Inicia lógica de parcelamento com cálculo parcial de multa
    - Iniciada a implementação da lógica de parcelamento
    - Cálculo inicial de multa por parcela (1%) adicionado
    - Resultados parciais exibidos no <select> para visualização
    - Ajustados nomes de variáveis para maior clareza
    - Lógica da entrada mantida com pequenas melhorias
    - Funcionalidade do parcelamento ainda em desenvolvimento

--- script.js
+++ script.js
@@ -53,24 +53,45 @@ botao_calcular.addEventListener('click', () => {
 
     if (tem_entrada.checked) {
         let data_do_vencimento_entrada = document.querySelector('#vencimento_da_entrada')
-        let venceu_dia = new Date(data_do_vencimento_entrada.value)
-        venceu_dia.setHours(0, 0, 0, 0)
+        let entrada_venceu_dia = new Date(data_do_vencimento_entrada.value)
+        entrada_venceu_dia.setHours(0, 0, 0, 0)
 
         let valor_da_entrada = document.querySelector('#valor_da_entrada').value
         let pagou_entrada = parseFloat(valor_da_entrada.replace(',','.'))
 
-        let atraso_da_entrada = (pagou_dia.getTime() - venceu_dia.getTime()) / (1000 * 60 * 60 * 24)
+        let atraso_da_entrada = (pagou_dia.getTime() - entrada_venceu_dia.getTime()) / (1000 * 60 * 60 * 24)
 
         let multa_da_entrada = (1*pagou_entrada/100)
         let juros_da_entrada = ((0.1*pagou_entrada/100) * (atraso_da_entrada))
 
         let total_da_entrada = (multa_da_entrada+juros_da_entrada)
-        let p1 = window.document.querySelector('#teste')
+        let p1 = window.document.querySelector('#teste_1')
 
         p1.innerHTML = `Dias de vencimento: ${atraso_da_entrada} Dias <br> Multa ${formatador.format(multa_da_entrada)} <br>
         Juros ${formatador.format(juros_da_entrada)} <br>
         Total: ${formatador.format(total_da_entrada)}`
         
+    }
+
+    if (tem_parcelamento.checked) {
+         let data_do_vencimento_parcelamento = document.querySelector('#vencimento_do_parcelamento')
+         let parcelamento_venceu_dia = new Date (data_do_vencimento_parcelamento.value)
+         parcelamento_venceu_dia.setHours(0, 0, 0, 0)
+
+         let parcelas = document.querySelector("#quantidade_de_parcelas")
+         let quantidade_de_parcelas = parseInt(parcelas.value)
+
+        let valor_das_parcelas = document.querySelector("#valor_das_parcelas").value
+        let pagou_parcelas = parseFloat(valor_das_parcelas.replace(',','.'))
+
+
+        let resultado_do_parcelamento = document.querySelector("#seltab")
+        for (let c = 1; c <= quantidade_de_parcelas; c++) {
+            let item = document.createElement('option')
+            let multa = (1*pagou_parcelas/100)
+            item.text = `Parcela é ${valor_das_parcelas} e a multa é ${multa}`
+            resultado_do_parcelamento.appendChild(item)
+        }
     }
 
 })
