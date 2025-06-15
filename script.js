60da273403c5b6f13487b04513df53330101977d
Adriano Costa <159786050+adrianocosta01@users.noreply.github.com>
2025-06-14 21:18:28 -0300

    Finalizado o cálculo e exibição da entrada, incluindo validação de datas, cálculo de multa e juros, e formatação monetária. Preparação da estrutura para o cálculo de parcelamento.

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ nao_tem_parcelamento.addEventListener('change', () => {
     }
 })
 
-//Calculos
+//INICIANDO OS CALCULOS
 let dia_do_pagamento = document.querySelector('#dia_do_pagamento')
 let botao_calcular = document.querySelector('#botao_calcular')
 
@@ -51,26 +51,81 @@ botao_calcular.addEventListener('click', () => {
     let pagou_dia = new Date(dia_do_pagamento.value)
     pagou_dia.setHours(0, 0, 0, 0)
 
+    //CABEÇALHO DA TABELA APARECE OU NÃO
+
+    //SELECIONAR A TABELA E CRIAR THEAD E TBODY
+    let tabela = document.querySelector("#tabela")
+    
+    let thead = document.createElement("thead")
+    let tbody = document.createElement("tbody")
+
+
+
+    if (tem_entrada.checked || tem_parcelamento.checked) {
+
+        //CRIANDO AS CELULAS DO TITULO
+        const colunas = [
+            "PARCELAS",
+            "DIAS DE ATRASO",
+            "VALOR DAS PRESTAÇÕES",
+            "VALOR DA MULTA",
+            "VALOR DO JUROS",
+            "JUROS + MULTA",
+            "TOTAL DEVIDO"]
+
+        //CRIAR A LINHA DO CABEÇALHO
+            let linha_cabecalho = document.createElement("tr")
+            colunas.forEach(valores_coluna => {
+                let th = document.createElement("th")
+                th.textContent = valores_coluna
+                linha_cabecalho.appendChild(th)
+            })
+
+            thead.appendChild(linha_cabecalho)
+            tabela.appendChild(thead)
+    }
+
+    //CALCULANDO A ENTRADA
     if (tem_entrada.checked) {
-        let data_do_vencimento_entrada = document.querySelector('#vencimento_da_entrada')
-        let entrada_venceu_dia = new Date(data_do_vencimento_entrada.value)
-        entrada_venceu_dia.setHours(0, 0, 0, 0)
+        let data_do_vencimento_entrada = document.querySelector("#vencimento_da_entrada")
+        let vencimento_entrada = new Date(data_do_vencimento_entrada.value)
+        vencimento_entrada.setHours(0, 0, 0, 0)
 
-        let valor_da_entrada = document.querySelector('#valor_da_entrada').value
-        let pagou_entrada = parseFloat(valor_da_entrada.replace(',','.'))
+        let pegar_valor_da_entrada = document.querySelector("#valor_da_entrada").value
+        let valor_da_entrada = parseFloat(pegar_valor_da_entrada.replace(',','.'))
 
-        let atraso_da_entrada = (pagou_dia.getTime() - entrada_venceu_dia.getTime()) / (1000 * 60 * 60 * 24)
+        let dias_de_atraso_da_entrada_calculo = pagou_dia.getTime() - vencimento_entrada.getTime()
+        let dias_de_atraso_da_entrada_resultado = (dias_de_atraso_da_entrada_calculo / (1000 * 60 * 60 * 24))
 
-        let multa_da_entrada = (1*pagou_entrada/100)
-        let juros_da_entrada = ((0.1*pagou_entrada/100) * (atraso_da_entrada))
+        let valor_da_multa_entrada = (1*valor_da_entrada/100)
 
-        let total_da_entrada = (multa_da_entrada+juros_da_entrada)
-        let p1 = window.document.querySelector('#teste_1')
-
-        p1.innerHTML = `Dias de vencimento: ${atraso_da_entrada} Dias <br> Multa ${formatador.format(multa_da_entrada)} <br>
-        Juros ${formatador.format(juros_da_entrada)} <br>
-        Total: ${formatador.format(total_da_entrada)}`
+        let valor_do_juros_entrada =  ((0.10*valor_da_entrada/100) * (dias_de_atraso_da_entrada_resultado))
         
+        let juros_com_multa_entrada = (valor_da_multa_entrada + valor_do_juros_entrada)
+        
+        let total_devido_entrada = (valor_da_entrada + valor_da_multa_entrada + valor_do_juros_entrada)
+    
+
+        let colunas = [
+            'ENTRADA',
+            dias_de_atraso_da_entrada_resultado,
+            valor_da_entrada,
+            valor_da_multa_entrada,
+            valor_do_juros_entrada,
+            juros_com_multa_entrada,
+            total_devido_entrada
+        ]
+
+        let linha_da_entrada = document.createElement("tr") 
+        colunas.forEach((valor, indice) => {
+            let celula = document.createElement("td")
+            celula.textContent = indice >= 2 ? formatador.format(valor) : valor
+            linha_da_entrada.appendChild(celula)
+        })
+
+        tbody.appendChild(linha_da_entrada)
+        tabela.appendChild(tbody)
+
     }
 
     if (tem_parcelamento.checked) {
